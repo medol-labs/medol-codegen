9234c78046e6bc3a737b3a21ab59b068288aed31
Bryce <chubalh@live.com>
2026-05-18 16:48:25 +0800

    feat(refine): Modify supabase query response change snakekeys to camelkeys

--- .generator/refine/app/templates/root/src/providers/data.ts
+++ .generator/refine/app/templates/root/src/providers/data.ts
@@ -1,4 +1,48 @@
+import type {
+  BaseRecord,
+  DataProvider,
+  GetListParams,
+  GetListResponse,
+  GetManyParams,
+  GetManyResponse,
+  GetOneParams,
+  GetOneResponse,
+} from "@refinedev/core";
 import { dataProvider as supabaseDataProvider } from "@refinedev/supabase";
+import camelcaseKeys from "camelcase-keys";
 import { supabaseClient } from "./supabase-client";
 
-export const dataProvider = supabaseDataProvider(supabaseClient);
+const provider = supabaseDataProvider(supabaseClient) as Required<DataProvider>;
+
+const camelcaseData = async <T extends { data?: unknown }>(
+  promise: Promise<T>,
+): Promise<T> => {
+  const response = await promise;
+
+  if (!("data" in response)) {
+    return response;
+  }
+
+  return {
+    ...response,
+    data: camelcaseKeys(response.data as Record<string, unknown>[], {
+      deep: true,
+    }) as T["data"],
+  };
+};
+
+export const dataProvider: Required<DataProvider> = {
+  ...provider,
+  getList: <TData extends BaseRecord = BaseRecord>(
+    params: GetListParams,
+  ): Promise<GetListResponse<TData>> =>
+    camelcaseData(provider.getList<TData>(params)),
+  getMany: <TData extends BaseRecord = BaseRecord>(
+    params: GetManyParams,
+  ): Promise<GetManyResponse<TData>> =>
+    camelcaseData(provider.getMany<TData>(params)),
+  getOne: <TData extends BaseRecord = BaseRecord>(
+    params: GetOneParams,
+  ): Promise<GetOneResponse<TData>> =>
+    camelcaseData(provider.getOne<TData>(params)),
+};
